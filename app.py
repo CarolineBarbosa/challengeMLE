75e93cc8d36349a2d8b9962045f990ede9fb7a49
carol <caroline.c.barbosa@accenture.com>
2024-09-22 21:27:01 -0300

    change in the app main page and post

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import logging
 from datetime import datetime
 from model.Model import train_model, load_config
+from fastapi.responses import HTMLResponse
 
 
 ### INITIATING LOG
@@ -48,17 +49,52 @@ class PredictionRequest(BaseModel):
     longitude: float
 
 
-@app.post("/predict/")
-async def predict(request: PredictionRequest, api_key: str = Depends(get_api_key)):
-    logging.info("Received prediction request: %s", request.json())
-    input_data = pd.DataFrame([request.dict()])
+# @app.post("/predict/")
+# async def predict(request: PredictionRequest, api_key: str = Depends(get_api_key)):
+#     logging.info("Received prediction request: %s", request.json())
+#     input_data = pd.DataFrame([request.dict()])
+
+#     prediction = model_pipeline.predict(input_data)
+#     prediction = round(prediction[0], 2)
+#     logging.info("Prediction request returned: CLP $ %s", prediction)
+#     return {"Predicted Price CLP $ ": prediction}
+
+@app.post("Price Prediction")
+async def predict(
+    type: str,
+    sector: str,
+    net_usable_area: float,
+    net_area: float,
+    n_rooms: float,
+    n_bathroom: float,
+    latitude: float,
+    longitude: float,
+    api_key: str = Depends(get_api_key)
+):
+    # Create DataFrame from individual parameters
+    input_data = pd.DataFrame([{
+        "type": type,
+        "sector": sector,
+        "net_usable_area": net_usable_area,
+        "net_area": net_area,
+        "n_rooms": n_rooms,
+        "n_bathroom": n_bathroom,
+        "latitude": latitude,
+        "longitude": longitude
+    }])
 
     prediction = model_pipeline.predict(input_data)
     prediction = round(prediction[0], 2)
     logging.info("Prediction request returned: CLP $ %s", prediction)
-    return {"Predicted Price CLP $ ": prediction}
+    return {f"Predicted Price CLP $ {prediction}"}
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    msg = """
+    <h2>Welcome to the Property-Friends Real Estate API!</h2>
+    <p>To get a property price prediction, go to the <a href="/docs">/docs</a> endpoint for an interactive API documentation.</p>
+    <p>To directly make predictions, use the <b>Price Prediction Post/</b> endpoint. Select 'Try it out' and include the necessary property details in the request body before executing the price prediction.</p>
+    <p>To get a property price prediction, you need an API key. Please, make sure you have access and include your API key to get autorization when making requests.</p>
+    """
+    return msg
